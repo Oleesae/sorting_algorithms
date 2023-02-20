671c3036fc48372d7030ab9fd80d5b5f7c21dbb3
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-02-20 22:28:48 +0100

    update README.md

---
+++ 0-bubble_sort.c
@@ -0,0 +1,23 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array using the bubble sort algorithm
+ * @array: array to be sorted
+ * @size: size of the array
+ *
+ * Return: void
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	/**
+	 * The size of the array would be monitored
+	 * then traverse through the array and look
+	 * for the largest value, compare the first
+	 * index with the next index. The comparison
+	 * would move from the index 0 to index [length
+	 * of the array - 2]. this is because the size
+	 * starts from 1 and the index starts from 0.
+	 * Next anytime the
+*/
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-0x1B. C - Sorting algorithms & Big O
+# 0x1B. C - Sorting algorithms & Big O
+
+### Collaborators:
+- Eboka Olisaemeka - email: ebokaolisaemeka@gmail.com, twitter: @elfiekki, Linkedn: Eboka Olisaemeka
+- Gabriel Ifeanyi -
